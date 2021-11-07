8e0581d624f15904450acb39b0db21c475d5e17b
Issey19 <aaaif342@gmail.com>
2021-11-07 11:10:59 +0000

    user_update

--- meshiterro/db/migrate/20211106122030_create_post_images.rb
+++ meshiterro/db/migrate/20211106122030_create_post_images.rb
@@ -1,4 +1,4 @@
-class CreatePostImages < ActiveRecord::Migration[5.0]
+class CreatePostImages < ActiveRecord::Migration[5.2]
   def change
     create_table :post_images do |t|
       t.text :shop_name
